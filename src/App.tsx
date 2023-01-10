5948e6ec31bc832001bb299c9d85a6054dbb8e33
Sal Rahman <salehen.rahman@gmail.com>
2023-01-10 10:02:32 -0800

    Got an example to show camera position

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import { useRef, useState, Ref, useReducer, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
+import { useRef, useState, useReducer, useEffect } from "react";
 import "./App.css";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { OrthographicCamera } from "@react-three/drei";
+
+function scalarMul<V extends number[]>(constant: number, vector: V): V {
+	return vector.map((v) => v * constant) as V;
+}
+
+function vector2DAdd(
+	a: [number, number],
+	b: [number, number]
+): [number, number] {
+	return [a[0] + b[0], a[1] + b[1]];
+}
+
+function vector2DSub(
+	a: [number, number],
+	b: [number, number]
+): [number, number] {
+	return vector2DAdd(a, scalarMul(-1, b));
+}
+
+function hadamard2D(
+	a: [number, number],
+	b: [number, number]
+): [number, number] {
+	return [a[0] * b[0], a[1] * b[1]];
+}
+
+function invertY(v: [number, number]): [number, number] {
+	return hadamard2D(v, [1, -1]);
+}
 
 function App() {
 	const [isMouseDown, setIsMouseDown] = useState(false);
@@ -23,6 +51,7 @@ function App() {
 		}),
 		{ zoom: zoomRef.current, cameraPosition: cameraPositionRef.current }
 	);
+	const [isClickingObject, setIsClickingObject] = useState(false);
 
 	useEffect(() => {
 		function run() {
@@ -55,15 +84,6 @@ function App() {
 
 						const newZoom = zoomRef.current - e.deltaY * 0.001;
 
-						const positionCanvas: [number, number] = [
-							(mousePositionRef.current[0] - rect.width / 2) /
-								Math.E ** zoomRef.current +
-								cameraPositionRef.current[0],
-							-(mousePositionRef.current[1] - rect.height / 2) /
-								Math.E ** zoomRef.current +
-								cameraPositionRef.current[1],
-						];
-
 						const mousePositionCentered = [
 							mousePositionRef.current[0] - rect.width / 2,
 							-(mousePositionRef.current[1] - rect.height / 2),
@@ -78,8 +98,6 @@ function App() {
 							newMousePosition[0] - mousePositionCentered[0],
 							newMousePosition[1] - mousePositionCentered[1],
 						];
-
-						console.log(displacement);
 
 						const newCameraPosition: [number, number] = [
 							cameraPositionRef.current[0] +
@@ -107,17 +125,23 @@ function App() {
 						mousePositionRef.current[0] = e.clientX - rect.left;
 						mousePositionRef.current[1] = e.clientY - rect.top;
 
-						const positionCanvas: [number, number] = [
-							(mousePositionRef.current[0] - rect.width / 2) /
-								Math.E ** zoomRef.current +
-								cameraPositionRef.current[0],
-							-(mousePositionRef.current[1] - rect.height / 2) /
-								Math.E ** zoomRef.current +
-								cameraPositionRef.current[1],
+						const mousePositionCentered = [
+							mousePositionRef.current[0] - rect.width / 2,
+							-(mousePositionRef.current[1] - rect.height / 2),
 						];
+
+						const mousePositionZoomed = [
+							mousePositionCentered[0] / Math.E ** zoomRef.current,
+							mousePositionCentered[1] / Math.E ** zoomRef.current,
+						] satisfies [number, number];
+
+						const mousePositionCamera = vector2DAdd(
+							mousePositionZoomed,
+							cameraPositionRef.current
+						);
 					}
 
-					if (isMouseDown) {
+					if (isMouseDown && !isClickingObject) {
 						cameraPositionRef.current = [
 							cameraPositionRef.current[0] -
 								e.movementX / Math.E ** zoomRef.current,
@@ -127,9 +151,17 @@ function App() {
 					}
 				}}
 			>
-				<mesh>
+				<mesh
+					onPointerDown={() => {
+						setIsClickingObject(true);
+					}}
+					onPointerUp={() => {
+						setIsClickingObject(false);
+					}}
+					position={[0, 0, 0]}
+				>
 					<meshBasicMaterial color={"red"} />
-					<sphereBufferGeometry args={[1, 30, 30]} />
+					<sphereBufferGeometry args={[0.5, 30, 30]} />
 				</mesh>
 				<gridHelper rotation={[Math.PI / 2, 0, 0]} />
 				<OrthographicCamera
